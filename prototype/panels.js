d474f1b2db57b956c05ffb58556cdda53185c9eb
Brian Kardell <bkardell@gmail.com>
2015-03-24 14:31:25 -0400

    Update panels.js
    Add matches polyfill

--- prototype/panels.js
+++ prototype/panels.js
@@ -1,3 +1,18 @@
+this.Element && function(ElementPrototype) {
+	ElementPrototype.matchesSelector = ElementPrototype.matchesSelector || 
+	ElementPrototype.mozMatchesSelector ||
+	ElementPrototype.msMatchesSelector ||
+	ElementPrototype.oMatchesSelector ||
+	ElementPrototype.webkitMatchesSelector ||
+	function (selector) {
+		var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
+ 
+		while (nodes[++i] && nodes[i] != node);
+ 
+		return !!nodes[i];
+	}
+}(Element.prototype);
+
 (function(document) {
     var lastUid = 0,
         nextUid = function() {
